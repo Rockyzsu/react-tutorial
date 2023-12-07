763a44918fd13c0472a5e8c6fc32ea9619375b4b
Rockyzsu <jinweizsu@gmail.com>
2023-12-08 01:32:36 +0800

    update

---
+++ learning_react/github-user/src/App.js
@@ -0,0 +1,6 @@
+import GitHubUser from "./fetch-component";
+function App() {
+  return <GitHubUser login={"rockyzsu"} />;
+}
+
+export default App;
---
+++ learning_react/github-user/src/fetch-component.js
@@ -0,0 +1,58 @@
+import { useState,useEffect } from "react";
+
+export default function GitHubUser({login}) {
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSucess={UserDetails}
+    />
+  );
+}
+
+function UserDetails({ data }) {
+  return (
+    <div className="githubuser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        <p>
+          {data.name} - {data.location}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Fetch(
+  {uri,
+  renderSucess,
+  loadingFallback = <p>loading....</p>,
+  renderError = (error) => <pre>{JSON.stringify(error)}</pre>
+}
+) {
+  const { loading, data, err } = useFetch(uri);
+  if (loading) return loadingFallback;
+  if (err) return renderError(err);
+  if (data) return renderSucess({ data });
+}
+
+//let uri = `https://api.github.com/users/${login}`
+
+function useFetch(uri) {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!uri) return;
+
+    console.log("enter into efffect");
+    fetch(uri)
+      .then((res) => res.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch((err) => setError(err));
+  }, [uri]);
+
+  return { loading, data, error };
+}
