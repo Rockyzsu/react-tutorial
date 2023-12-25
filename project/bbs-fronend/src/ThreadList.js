f146a1a1d359d877e4eff39bfc6ab93c6d0e87cc
Rocky Chen <jinweizsu@gmail.com>
2023-12-25 21:20:25 +0800

    udpate

--- project/bbs-fronend/src/ThreadList.js
+++ project/bbs-fronend/src/ThreadList.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { Table } from 'react-bootstrap';
 
-function Thread({ title, author }) {
+function Thread({ title, content, author }) {
   return (
     <>
       <tr>
         <td>{title}</td>
+        <td>{content}</td>
         <td>{author}</td>
       </tr>
     </>
@@ -21,14 +23,18 @@ export default function ThreadList() {
   //     {'title': '标题4', 'author': '作者4'},
   //     {'title': '标题5', 'author': '作者5'},
   // ]
+
   const [threadContent,setThreadContent] = useState([]); // ['title': '标题1', 'author': '作者1'
   useEffect(()=>{
     async function fetchThreadContent(){
       const resp = await fetch(
         'http://localhost:7000/api/threads'
       )
-      const data = await resp.json();
-      setThreadContent(data.data);
+      console.log(resp.ok);
+      console.log(resp.message  );
+      const resp_data = await resp.json();
+      console.log(resp_data.data);
+      setThreadContent(resp_data.data);
 
     }
     fetchThreadContent();
@@ -36,18 +42,20 @@ export default function ThreadList() {
   // const threadContent = getThreadContent();
 
   return (
-    <table border="1">
-      <thead>
+    <Table striped hover bordered>
+        <thead>
         <tr>
           <th>标题</th>
+          <th>内容</th>
           <th>作者</th>
         </tr>
       </thead>
+
       <tbody>
-        {threadContent.map((item) => {
-          return <Thread title={item.title} author={item.author} />;
+        {threadContent.map((item,index) => {
+          return <Thread key={index} title={item.title} content={item.content} author={item.author.username}/>;
         })}
       </tbody>
-    </table>
+    </Table>
   );
 }
