d9b7d8bf337548d258ff2d4bfa6f37fb1d6d7b94
Rocky Chen <jinweizsu@gmail.com>
2023-12-26 11:43:41 +0800

    zero react

---
+++ learning_react/manual-create-react/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>公众号：AI应用全栈工程师</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
