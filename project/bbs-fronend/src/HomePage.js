9fe7f857bfbe9bd249adc6fa5d4c7b4146759a55
Rockyzsu <jinweizsu@gmail.com>
2024-03-18 00:01:39 +0800

    home

--- project/bbs-fronend/src/HomePage.js
+++ project/bbs-fronend/src/HomePage.js
@@ -1,4 +1,17 @@
+import { Container,Row,Col } from "react-bootstrap";
+
 const HomePage =()=>{
+return (
+    <Container fluid>
+        <Row>
+            <Col sm={12} md={7} className="pr-1">
+                <Demo/>
+            </Col>
+            
+        </Row>
+    </Container>
+)
+    
 
 }
 
